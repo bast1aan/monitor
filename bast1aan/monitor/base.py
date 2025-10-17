7b6ad22e7d7c2117c4d5543457ed81e1e33cf20e
Bastiaan Welmers <bastiaan@welmers.net>
2025-10-17 15:59:48 +0200

    Make CommandSet compatible with Command

--- bast1aan/monitor/base.py
+++ bast1aan/monitor/base.py
@@ -2,11 +2,21 @@ from __future__ import annotations
 import subprocess
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Tuple, Iterator
+from typing import Tuple, Iterator, Iterable
+
+
+class CommandResult(ABC):
+    command: Command
+    @property
+    def error(self) -> bool: ...
+    @property
+    def __bool__(self) -> bool: ...
+    @property
+    def __str__(self) -> str: ...
 
 
 @dataclass
-class CommandResult:
+class _CommandResult(CommandResult):
     ok: bool
     msg: str
     command: Command
@@ -22,11 +32,11 @@ class CommandResult:
         return self.msg
 
     @classmethod
-    def Ok(cls, msg: str, command: Command) -> "CommandResult":
+    def Ok(cls, msg: str, command: Command) -> CommandResult:
         return cls(True, msg, command)
 
     @classmethod
-    def Error(cls, msg: str, command: Command) -> "CommandResult":
+    def Error(cls, msg: str, command: Command) -> CommandResult:
         return cls(False, msg, command)
 
 
@@ -47,19 +57,42 @@ class ExecutorCommand(Command):
         msg = b'\n'.join((result.stdout or b'', result.stderr or b'')).decode()
 
         if result.returncode != 0:
-            return CommandResult.Error(msg, self)
+            return _CommandResult.Error(msg, self)
         else:
-            return CommandResult.Ok(msg, self)
+            return _CommandResult.Ok(msg, self)
 
     def __str__(self) -> str:
         return self.command
 
 
-class CommandSet:
+@dataclass
+class CommandSetResult(CommandResult, Iterable[CommandResult]):
+    command: CommandSet
+    _results: tuple[CommandResult, ...] = ()
+
+    def _walk(self) -> Iterator[CommandResult]:
+        results = []
+        for command in self.command.commands:
+            result = command()
+            results.append(result)
+            yield result
+        self._results = tuple(results)
+
+    def __iter__(self) -> Iterator[CommandResult]:
+        return iter(self._results) if self._results else self._walk()
+
+    def __bool__(self) -> bool:
+        return all(result for result in iter(self))
+
+    def __str__(self) -> str:
+        return '\n'.join((str(result) for result in iter(self)))
+
+
+class CommandSet(Command):
     commands: Tuple[Command, ...]
     def __init__(self, *commands: Command):
         self.commands = commands
-    def __call__(self) -> Iterator[CommandResult]:
-        for command in self.commands:
-            yield command()
-
+    def __call__(self) -> CommandSetResult:
+        return CommandSetResult(command=self)
+    def __str__(self) -> str:
+        return '\n'.join((str(command) for command in self.commands))
