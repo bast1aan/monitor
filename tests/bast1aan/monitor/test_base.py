d7296fafec2ac98b19901b47ff2b971fa932e51b
Bastiaan Welmers <bastiaan@welmers.net>
2025-10-17 16:34:21 +0200

    Make test reflect changes

--- tests/bast1aan/monitor/test_base.py
+++ tests/bast1aan/monitor/test_base.py
@@ -41,20 +41,34 @@ def test_commandset() -> None:
         PingCommand('127.0.0.1', only=IPV6),
         PingCommand('127.0.0.1', only=IPV4),
     )
-    result_set = [result for result in command_set()]
-    assert tuple(bool(result) for result in result_set) == (True, True, False, False, True)
+    result_set = command_set()
+    result_list = list(result_set)
+    assert tuple(bool(result) for result in result_list) == (True, True, False, False, True)
 
-    assert '64 bytes from localhost (127.0.0.1): icmp_seq=1' in str(result_set[0])
-    assert '1 packets transmitted, 1 received, 0% packet loss' in str(result_set[0])
+    assert '64 bytes from localhost (127.0.0.1): icmp_seq=1' in str(result_list[0])
+    assert '1 packets transmitted, 1 received, 0% packet loss' in str(result_list[0])
 
-    assert '64 bytes from localhost (::1): icmp_seq=1' in str(result_set[1])
-    assert '1 packets transmitted, 1 received, 0% packet loss' in str(result_set[1])
+    assert '64 bytes from localhost (::1): icmp_seq=1' in str(result_list[1])
+    assert '1 packets transmitted, 1 received, 0% packet loss' in str(result_list[1])
 
-    assert 'flupflops: Name or service not known' in str(result_set[2])
-    assert 'ping -c 1 flupflops' == str(result_set[2].command)
+    assert 'flupflops: Name or service not known' in str(result_list[2])
+    assert 'ping -c 1 flupflops' == str(result_list[2].command)
 
-    assert '127.0.0.1: Address family for hostname not supported' in str(result_set[3])
-    assert 'ping -c 1 -6 127.0.0.1' == str(result_set[3].command)
+    assert '127.0.0.1: Address family for hostname not supported' in str(result_list[3])
+    assert 'ping -c 1 -6 127.0.0.1' == str(result_list[3].command)
 
-    assert '64 bytes from 127.0.0.1: icmp_seq=1' in str(result_set[4])
-    assert '1 packets transmitted, 1 received, 0% packet loss' in str(result_set[4])
+    assert '64 bytes from 127.0.0.1: icmp_seq=1' in str(result_list[4])
+    assert '1 packets transmitted, 1 received, 0% packet loss' in str(result_list[4])
+
+    assert bool(result_set) is False
+
+def test_commandset_success() -> None:
+    command_set = CommandSet(
+        PingCommand('localhost', only=IPV4),
+        PingCommand('localhost', only=IPV6),
+        PingCommand('127.0.0.1', only=IPV4),
+    )
+
+    result_set = command_set()
+
+    assert bool(result_set) is True
