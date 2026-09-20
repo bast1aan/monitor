d7b7b991a46a29fc89b845d731f3ad7fe0386cec
Bastiaan Welmers <bastiaan@welmers.net>
2026-09-20 15:17:58 +0200

    Fix message in case of error

--- bast1aan/monitor/ping.py
+++ bast1aan/monitor/ping.py
@@ -39,8 +39,10 @@ class PingCommand(ExecutorCommand):
         _previous_runs[self] = time.time()
 
     def _format_msg(self, stdout: bytes, stderr: bytes) -> str:
-        msg = super()._format_msg(stdout, stderr)
-        try:
-            return msg.split("ping statistics ---\n")[1].split("\n")[0].strip()
-        except Exception:
-            return msg
+        if not stderr:
+            try:
+                return stdout.decode().split("ping statistics ---\n")[1].split("\n")[0].strip()
+            except Exception:
+                return super()._format_msg(stdout, stderr).replace('\n', ' ')
+        else:
+            return stderr.replace(b'\n', b' ').decode()
